1d8f4dc8e642923912f0ef7933c0f7479cd621a4
Alex Bourne <afb989@outlook.com>
2022-05-27 15:32:12 -0400

    completed project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,58 @@
 import "./App.css";
-import React from "react";
-import { useState } from "react";
-import Form from "./components/Form";
-import Answers from "./components/Answers";
+import React, { useState, useRef } from "react";
+import { motion, useAnimation } from "framer-motion";
+
+const fetchAnswer = async (question) => {
+  let params = encodeURIComponent(question);
+  let uri = "https://8ball.delegator.com/magic/JSON/" + params;
+  const response = await fetch(uri);
+  const json = await response.json();
+  return json.magic.answer;
+};
 
 function App() {
+  const controls = useAnimation();
+  const answerRef = useRef(-1);
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const getAnswer = async (e) => {
+    e.preventDefault();
+    if (!question) {
+      return;
+    }
+    controls.start({ x: [0, 75, -75, 50, -25, 0] });
+    setQuestion("");
+    answerRef.value = await fetchAnswer(question);
+  };
+
   return (
     <div className="App">
       <h1>Magic 8 Ball</h1>
-      <Answers />
-      <img id="eightball" src="8ball-lrg.png" alt="Magic 8 Ball" />
-      <Form setQuestion={Form.question} setAnswer={Form.answer} />
+
+      <div>
+        <form onSubmit={getAnswer}>
+          <div className="form-group">
+            <input
+              id="questionInput"
+              className="form-control"
+              type="text"
+              placeholder="Type your question, then click the Magic 8 Ball"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+            />
+          </div>
+          <motion.button
+            animate={controls}
+            onAnimationComplete={() => setAnswer(answerRef.value)}
+            type="submit"
+            id="submit"
+          >
+            <img id="eightball" src="8ball-lrg.png" alt="Magic 8 Ball" />
+          </motion.button>
+        </form>
+        <h2 className="eight-ball-reply">{answer}</h2>
+      </div>
     </div>
   );
 }
--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,25 +1,7 @@
-const Answers = () => {
-  const answers = [
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "Yes definitely",
-    "You may rely on it",
-    "Reply hazy, try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",
-    "Don't count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful",
-  ];
+// import { randomAnswer } from "./Form";
 
-  let randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+// const Answers = () => {
 
-  return <h3 className="eight-ball-reply">{randomAnswer}</h3>;
-};
+// };
 
-export default Answers;
+// export default Answers;
